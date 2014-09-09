29f04d28af2c95268615c76ede3b9c5adbcc722f
wampir1408 <asiafurmaniak@wp.pl>
2014-09-09 13:25:06 +0200

    Obraz1
    Stworzenie szkieletu modelu obrazu

---
+++ szkielet/obraz.h
@@ -0,0 +1,33 @@
+#ifndef OBRAZ_H
+#define OBRAZ_H
+
+int obrazVertexCount=4;
+int rama_oVertexCount=32;
+
+float obrazVertices[]={
+  -0.7, -0.7, 0, 
+  0.7, -0.7, 0, 
+  0.7, 0.7, 0, 
+  -0.7, 0.7, 0
+};
+
+float rama_oVertices[]={
+
+};
+
+
+float rama_oColors[]={
+  
+};
+
+float obrazColors[]={
+  1,0,0, 1,0,0, 1,0,0, 1,0,0
+};
+
+float rama_oTexVertices[]={
+  
+};
+
+float obrazTexVertices[]={
+  0,0,	0,1,	1,1,	1,0
+};
